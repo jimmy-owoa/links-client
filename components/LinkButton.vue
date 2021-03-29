d6bda37469b1cebbca8f11e972e755da7aa929e3
Jeremías <jimmyguionbajo@gmail.com>
2021-03-29 20:50:42 -0300

    Init

---
+++ components/LinkButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card elevation="3" height="60" min-width="480" hover light class="rounded-card" :href="link" target="_blank">
+    <v-row>
+      <v-col cols="2">
+        <v-img
+          :src="imgSrc"
+          height="40"
+          width="40"
+          class="ml-4"
+        ></v-img>
+      </v-col>
+      <v-col cols="8">
+        <v-flex text-md-center pt-2>{{text}}</v-flex>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    imgSrc: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: false
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row justify-sm="center" align-sm="center">
+    <v-col cols="7">
+      <v-img src="https://jeremiasgutierrez.cl/wp-content/uploads/2021/03/18475845.jpeg" height="100" contain></v-img>
+    </v-col>
+    <v-col cols="7">
+      <link-button imgSrc="https://jeremiasgutierrez.cl/wp-content/uploads/2020/06/favicon.ico" text="SITIO WEB FOTOGRAFÍA" link="https://jeremiasgutierrez.cl"/>
+    </v-col>
+    <v-col cols="7">
+      <link-button imgSrc="https://jeremiasgutierrez.cl/wp-content/uploads/2021/03/instagram.png" text="INSTAGRAM" link="https://www.instagram.com/jg_fotografia__"/>
+    </v-col>
+    <v-col cols="7">
+      <link-button imgSrc="https://jeremiasgutierrez.cl/wp-content/uploads/2021/03/WhatsApp-logo.png" text="WHATSAPP" link="https://wa.me/56951159600"/>
+    </v-col>
+    <v-col cols="7">
+      <link-button imgSrc="https://jeremiasgutierrez.cl/wp-content/uploads/2021/03/1024px-Telegram_2019_Logo.svg.png" text="TELEGRAM" link="https://t.me/jGutierrezA"/>
+    </v-col>
+    <v-col cols="7">
+      <link-button imgSrc="https://jeremiasgutierrez.cl/wp-content/uploads/2021/03/code.png" text="DESARROLLO WEB" link="https://ofalis.cl"/>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import LinkButton from '~/components/LinkButton.vue';
+
+export default {
+  components: {
+    LinkButton
+  },
+};
+</script>,
+
+<style scoped>
+.rounded-card{
+    border-radius: 50px;
+}
+</style>
